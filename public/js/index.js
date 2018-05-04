7b361e160a9ea3c63a261a6f4f9232b1645b5c62
ericlan0219 <eric@Erics-MacBook-Pro-2.local>
2018-05-03 22:06:55 -0700

    Updated UI Features

--- public/js/index.js
+++ public/js/index.js
@@ -1,25 +1,27 @@
 function login() {
   var un = $("#logUsr").val();
   var ps = $("#logPwd").val();
-  $.ajax({
-    type: 'GET',
-    url: '/login',
-    data: {
-      username: un,
-      password: ps
-    },
-    success: function(res) {
-      window.location.href = '/catalog';
-    },
-    statusCode: {
-      500: function() {
-        $("#logUsr").val("");
-        $("#logPwd").val("");
-        $("#logUsr").css("border-color", "red");
-        $("#logPwd").css("border-color", "red");
+  if (un != "" && ps != ""){
+    $.ajax({
+      type: 'GET',
+      url: '/login',
+      data: {
+        username: un,
+        password: ps
+      },
+      success: function(res) {
+        window.location.href = '/catalog';
+      },
+      statusCode: {
+        500: function() {
+          $("#logUsr").val("");
+          $("#logPwd").val("");
+          $("#logUsr").css("border-color", "red");
+          $("#logPwd").css("border-color", "red");
+        }
       }
-    }
-  });
+    });
+  }
 }
 
 function showForm(register) {
@@ -35,23 +37,29 @@ function showForm(register) {
 function register() {
   var un = $("#regUsr").val();
   var ps = $("#regPwd").val();
-  $.ajax({
-    type: 'GET',
-    url: '/register',
-    data: {
-      username: un,
-      password: ps
-    },
-    success: function(res) {
-      window.location.href = '/catalog';
-    },
-    statusCode: {
-      500: function() {
-        $("#regUsr").val("");
-        $("#regPwd").val("");
-        $("#regUsr").css("border-color", "red");
-        $("#regPwd").css("border-color", "red");
+  var em = $("#regEmail").val();
+  if (un != "" && ps != "" && em != ""){
+      $.ajax({
+        type: 'GET',
+        url: '/register',
+        data: {
+        username: un,
+        password: ps,
+        email: em
+      },
+      success: function(res) {
+        window.location.href = '/catalog';
+      },
+      statusCode: {
+        500: function() {
+          $("#regUsr").val("");
+          $("#regPwd").val("");
+          $("#regEmail").val("");
+          $("#regUsr").css("border-color", "red");
+          $("#regPwd").css("border-color", "red");
+          $("#regEmail").css("border-color", "red");
+        }
       }
-    }
-  });
+    });
+  }
 }
